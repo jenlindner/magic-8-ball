c5f5ce7a01c9295f5de1003aad08b5642c33a2a3
jenlindner <jen@jenlindner.com>
2010-05-26 15:43:29 -0500

    second

---
+++ server.js
@@ -0,0 +1,231 @@
+HOST = null; // localhost
+PORT = 8001;
+
+var fu = require("./fu"),
+    sys = require("sys"),
+    url = require("url"),
+    qs = require("querystring");
+
+var MESSAGE_BACKLOG = 200,
+    SESSION_TIMEOUT = 60 * 1000;
+    
+//create a user for magic 8-ball
+
+var magic = false;
+
+//randomize this - possibly store in object -
+//write a function to call that gets random element from array.
+var magic_text = new Array("Signs Point to no", "Definitely");
+
+
+var channel = new function () {
+  var messages = [],
+      callbacks = [];
+
+//
+
+  this.appendMessage = function (nick, type, text) {
+//   create_new_user();
+    var m = { nick: nick
+            , type: type // "msg", "join", "part"
+            , text: text
+            , timestamp: (new Date()).getTime()
+            };
+
+    switch (type) {
+      case "msg":
+      //if you want to become the magic 8-ball
+        if (text.indexOf("Magic 8-ball?") != -1 && magic == false){
+            magic = true;
+     //     channel.call_again("magic", "msg", "hello");
+            channel.appendMessage("magic", "msg", "What do you wish to know?"); 
+          //find way of not creating new magic 8-ball for each time someone types this. may need
+          //a magic 8-ball user to exist in sessions already, and just begin responding to questions when appropriate.
+ 
+          //sys.puts("What do wish you to know?");
+          
+          //how to simulate magic 8-ball?
+          //create a magic 8-ball new user that comes online and responds to any msg that contains a question mark 
+          //at the end.        
+       // }else if (m.nick == "Magic 8-ball") {
+          //the random magic 8ball answers to questions
+         // m.text += "\n Signs point to no.";
+          //sys.puts("<" + nick + "> " + text);
+       }else if (text.indexOf("?") != -1 && magic == true){
+         channel.appendMessage("magic", "msg", magic_text[0]); 
+       }else {
+          m.text = text;
+          sys.puts(text);
+        }
+        break;
+      case "join":
+        sys.puts(nick + " join");
+        break;
+      case "part":
+        sys.puts(nick + " part");
+        break;
+    }
+
+    messages.push( m );
+
+    while (callbacks.length > 0) {
+      callbacks.shift().callback([m]);
+    }
+
+    while (messages.length > MESSAGE_BACKLOG)
+      messages.shift();
+  };
+
+  this.query = function (since, callback) {
+    var matching = [];
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      if (message.timestamp > since)
+        matching.push(message)
+    }
+
+    if (matching.length != 0) {
+      callback(matching);
+    } else {
+      callbacks.push({ timestamp: new Date(), callback: callback });
+    }
+  };
+
+  // clear old callbacks
+  // they can hang around for at most 30 seconds.
+  setInterval(function () {
+    var now = new Date();
+    while (callbacks.length > 0 && now - callbacks[0].timestamp > 30*1000) {
+      callbacks.shift().callback([]);
+    }
+  }, 1000);
+};
+
+var sessions = {};
+
+function createSession (nick) {
+//  console.log("create session");
+  if (nick.length > 50) return null;
+  if (/[^\w_\-^!]/.exec(nick)) return null;
+
+  for (var i in sessions) {
+    var session = sessions[i];
+    if (session && session.nick === nick) return null;
+  }
+
+  var session = { 
+    nick: nick, 
+    id: Math.floor(Math.random()*99999999999).toString(),
+    timestamp: new Date(),
+
+    poke: function () {
+      session.timestamp = new Date();
+    },
+
+    destroy: function () {
+      channel.appendMessage(session.nick, "part");
+      delete sessions[session.id];
+    }
+  };
+
+  sessions[session.id] = session;
+  return session;
+}
+
+// interval to kill off old sessions
+setInterval(function () {
+  var now = new Date();
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id)) continue;
+    var session = sessions[id];
+
+    if (now - session.timestamp > SESSION_TIMEOUT) {
+      session.destroy();
+    }
+  }
+}, 1000);
+
+fu.listen(PORT, HOST);
+
+fu.get("/", fu.staticHandler("index.html"));
+fu.get("/style.css", fu.staticHandler("style.css"));
+fu.get("/client.js", fu.staticHandler("client.js"));
+fu.get("/jquery-1.2.6.min.js", fu.staticHandler("jquery-1.2.6.min.js"));
+
+
+fu.get("/who", function (req, res) {
+  var nicks = [];
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id)) continue;
+    var session = sessions[id];
+    nicks.push(session.nick);
+  }
+  res.simpleJSON(200, { nicks: nicks });
+});
+
+
+fu.get("/join", function (req, res) {
+  var nick = qs.parse(url.parse(req.url).query).nick;
+  if (nick == null || nick.length == 0) {
+    res.simpleJSON(400, {error: "Bad nick."});
+    return;
+  }
+  var session = createSession(nick);
+  if (session == null) {
+    res.simpleJSON(400, {error: "Nick in use"});
+    return;
+  }
+
+  //sys.puts("connection: " + nick + "@" + res.connection.remoteAddress);
+  //channel.appendMessage(session.nick, "create_magic");
+  if (session.nick != "magic"){
+    channel.appendMessage(session.nick, "join");
+}
+  res.simpleJSON(200, { id: session.id, nick: session.nick});
+});
+
+fu.get("/part", function (req, res) {
+  var id = qs.parse(url.parse(req.url).query).id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.destroy();
+  }
+  res.simpleJSON(200, { });
+});
+
+fu.get("/recv", function (req, res) {
+  if (!qs.parse(url.parse(req.url).query).since) {
+    res.simpleJSON(400, { error: "Must supply since parameter" });
+    return;
+  }
+  var id = qs.parse(url.parse(req.url).query).id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.poke();
+  }
+
+  var since = parseInt(qs.parse(url.parse(req.url).query).since, 10);
+
+  channel.query(since, function (messages) {
+    if (session) session.poke();
+    res.simpleJSON(200, { messages: messages });
+  });
+});
+
+fu.get("/send", function (req, res) {
+  var id = qs.parse(url.parse(req.url).query).id;
+  var text = qs.parse(url.parse(req.url).query).text;
+
+  var session = sessions[id];
+  if (!session || !text) {
+    res.simpleJSON(400, { error: "No such session id" });
+    return; 
+  }
+
+  session.poke();
+
+  channel.appendMessage(session.nick, "msg", text);
+  res.simpleJSON(200, {});
+});
